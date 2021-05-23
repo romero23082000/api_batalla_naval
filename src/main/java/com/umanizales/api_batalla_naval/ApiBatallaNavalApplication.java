c225662b81f7aa5e5e6054745ee99995d95d6b37
AndresR-coder <aromeroibague@gmailcom>
2021-05-23 14:20:27 -0500

    primer version api batalla naval conexion a base de datos

---
+++ src/main/java/com/umanizales/api_batalla_naval/ApiBatallaNavalApplication.java
@@ -0,0 +1,13 @@
+package com.umanizales.api_batalla_naval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBatallaNavalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBatallaNavalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/umanizales/api_batalla_naval/ApiBatallaNavalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umanizales.api_batalla_naval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBatallaNavalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
